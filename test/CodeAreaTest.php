b838a9b0037756a59edad627ae7d80695cfc9fa3
Vincent Wong <vincentwwy168@gmail.com>
2024-09-21 20:50:05 +0800

    Implement the tests for CodeArea

--- test/CodeAreaTest.php
+++ test/CodeAreaTest.php
@@ -3,16 +3,34 @@
 namespace Vectorial1024\OpenLocationCodePhp\Test;
 
 use PHPUnit\Framework\TestCase;
+use Vectorial1024\OpenLocationCodePhp\CodeArea;
 
 class CodeAreaTest extends TestCase
 {
+    /**
+     * A dummy CodeArea for testing; specifying the rectangular area of (0, 3) to (4, 8).
+     * @var CodeArea
+     */
+    private CodeArea $theArea;
+
     public function setUp(): void
     {
-        $this->assertTrue(true);
+        $this->theArea = new CodeArea(0, 3, 4, 8, 5);
     }
 
-    public function testSomething(): void
+    public function testCorrectLatitudeHeight(): void
     {
-        $this->assertTrue(true);
+        $this->assertEquals(4, $this->theArea->getLatitudeHeight());
+    }
+
+    public function testCorrectLongitudeWidth(): void
+    {
+        $this->assertEquals(5, $this->theArea->getLongitudeWidth());
+    }
+
+    public function testCorrectCenterCoordinates(): void
+    {
+        $this->assertEquals(2, $this->theArea->getCenterLatitude());
+        $this->assertEquals(5.5, $this->theArea->getCenterLongitude());
     }
 }
