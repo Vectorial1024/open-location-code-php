47ee033807fc5c1c14676a25965f95b8a2e18ec4
Vincent Wong <vincentwwy168@gmail.com>
2024-09-29 16:53:14 +0800

    Implement contains()

--- src/OpenLocationCode.php
+++ src/OpenLocationCode.php
@@ -341,6 +341,22 @@ final class OpenLocationCode implements Stringable
         return self::createFromCoordinates($recoveredLatitude, $recoveredLongitude, strlen($recovered->code) - 1);
     }
 
+    /**
+     * Returns whether the bounding box specified by the Open Location Code contains the provided point.
+     * 
+     * @param float $latitude The provided latitude in degrees.
+     * @param float $longitude The provided longitude in degrees.
+     * @return bool True if tge coordinates are contained by the code.
+     */
+    public function contains(float $latitude, float $longitude): bool
+    {
+        $codeArea = $this->decode();
+        return $codeArea->southLatitude <= $latitude
+            && $latitude < $codeArea->northLatitude
+            && $codeArea->westLongitude <= $longitude
+            && $longitude < $codeArea->eastLongitude;
+    }
+
     // ---
 
     /**
